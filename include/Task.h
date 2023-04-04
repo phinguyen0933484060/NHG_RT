c545b98b79398714a5b7ee580dfe5a503e3944fa
phinguyen0933484060 <nguyenlamphiyugi282001@gmail.com>
2023-04-04 07:37:00 -0400

    Mutex Task file

---
+++ include/Task.h
@@ -0,0 +1,14 @@
+// Task Lib
+#ifndef TASK_H
+#define TASK_H
+
+class NHG_Task{
+    private:
+        int ID;
+        int arrival_time;
+        int deadline;
+        int execution_time;
+    public:
+}
+
+#endif
